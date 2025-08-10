d4c9a3c41050fc9a78ae550499873fa3268d3a94
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2025-08-10 17:17:11 +0530

    2 version

--- Main.java
+++ Main.java
@@ -2,13 +2,12 @@
 
 import java.util.Date;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("Output from java project");
         System.out.println(new Date());
         System.out.println("Mohit Is a Genius");
+        System.out.println("We the people of India having solemnly resolved to constitute India into a sovereign socialistic and secular republic");
     }
 }
