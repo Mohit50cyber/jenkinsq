345f5ce316f8f97d72d646d17518c93bcbd8586b
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2025-08-12 09:57:22 +0530

    13 v

--- Main.java
+++ Main.java
@@ -26,6 +26,7 @@ public class Main {
         System.out.println("-----------------------------------------------------------------------");
         System.out.println("Hi I am Mohit and I work in publicis sapient");
         System.out.println("QWERTY");
+        System.out.println(new Date());
    
     }
 }
